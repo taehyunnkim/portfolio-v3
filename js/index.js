e81911b8e69690da94458360d43871a7d9a2bccb
Eric Kim <kim2000@uw.edu>
2020-02-21 23:52:28 -0800

    add footer

--- js/index.js
+++ js/index.js
@@ -32,12 +32,19 @@ const initHome = () => {
     });
 
     let intro = document.querySelector(".intro");
+    let footer = document.querySelector("footer")
     intro.style.opacity = "1";
 
     gsap.from(intro, 1, {
         opacity: 0,
         y: 50,
         ease: "power4.out"
+    })
+
+    gsap.to(footer, 1, {
+        opacity: 1,
+        ease: "power4.out",
+        delay: 1
     })
 }
 
